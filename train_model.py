19a82b96148dbde6c37ab6f6ed3524ad5d90c867
Darlie Henry <darlie0525@gmail.com>
2026-03-17 17:45:34 +0800

    Implemented the training for the model, it determines accuracy and using joblib to save and load the model

---
+++ train_model.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+data = pd.read_csv("dataset.csv")
+
+X = data[['packet_count', 'avg_packet_size', 'syn_ratio']]
+y = data['label']
+
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier()
+
+model.fit(X_train, y_train)
+
+
+accuracy = model.score(X_test, y_test)
+
+print("Model accuracy: ", accuracy)
+joblib.dump(model, "ddos_model.plk")
